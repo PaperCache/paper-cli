b8be2a8dc052d18fc17d28c468ee9c580e71ea5d
Kia Shakiba <kiashakiba@protonmail.com>
2023-06-22 16:51:23 -0400

    Add support for stats struct

--- src/command.rs
+++ src/command.rs
@@ -1,8 +1,14 @@
 pub mod error;
 pub mod parser;
 
-use paper_client::{PaperClient, PaperClientResponse, PaperClientError};
-use paper_client::Policy;
+use kwik::fmt;
+
+use paper_client::{
+	PaperClient,
+	PaperClientResponse,
+	PaperClientError,
+	Policy,
+};
 
 pub enum Command {
 	Ping,
@@ -35,7 +41,51 @@ impl Command {
 			Command::Resize(size) => client.resize(size).await,
 			Command::Policy(policy) => client.policy(policy).await,
 
-			Command::Stats => client.stats().await,
+			Command::Stats => {
+				let stats_response = client.stats().await?;
+				let stats = stats_response.data();
+
+				let max_size_output = format!(
+					"max_size:\t{} ({} B)",
+					fmt::memory(stats.get_max_size(), Some(2)),
+					stats.get_max_size()
+				);
+
+				let used_size_output = format!(
+					"used_size:\t{} ({} B)",
+					fmt::memory(stats.get_used_size(), Some(2)),
+					stats.get_used_size()
+				);
+
+				let total_gets_output = format!(
+					"total_gets:\t{}",
+					fmt::number(stats.get_total_gets())
+				);
+
+				let miss_ratio_output = format!(
+					"miss_ratio:\t{:.3}",
+					stats.get_miss_ratio()
+				);
+
+				let policy_output = format!(
+					"policy:\t\t{}",
+					stats.get_policy().id()
+				);
+
+				let stats_string = format!(
+					"paper stats\n{}\n{}\n{}\n{}\n{}",
+					max_size_output,
+					used_size_output,
+					total_gets_output,
+					miss_ratio_output,
+					policy_output
+				);
+
+				Ok(PaperClientResponse::new(
+					stats_response.is_ok(),
+					stats_string
+				))
+			},
 		}
 	}
 }
