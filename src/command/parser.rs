ab9d82a5a8d1938dfa37deec8f5707fedcd6550b
Kia Shakiba <kiashakiba@protonmail.com>
2025-05-29 18:40:47 -0400

    Add license

--- src/command/parser.rs
+++ src/command/parser.rs
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) Kia Shakiba
+ *
+ * This source code is licensed under the GNU AGPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 use std::str::FromStr;
 use regex::Regex;
 use parse_size::parse_size as parse_input_size;
